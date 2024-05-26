285f2f3410d4b389403cac40661c8d30f7aa6a74
hththai <huythaicmvn@gmail.com>
2024-05-26 16:17:07 +1000

    test

--- profile/obj/Debug/net8.0/profile.AssemblyInfo.cs
+++ profile/obj/Debug/net8.0/profile.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("94d60b44-6c05-4076-8d03-2f2c1d5877f1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("profile")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f9c5369dd3288bbf7123a7b34a0763d6976ce89")]
 [assembly: System.Reflection.AssemblyProductAttribute("profile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("profile")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
